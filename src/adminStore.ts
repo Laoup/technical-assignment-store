e20b5ff819914b276c23dd51e1a77eb3428d1c62
Alban Meurice <korvin.meurice@gmail.com>
2025-11-07 22:35:07 +0100

    Two test suites passed

--- src/adminStore.ts
+++ src/adminStore.ts
@@ -5,16 +5,16 @@ import { UserStore } from "./userStore";
 
 
 export class AdminStore extends Store {
-  @Restrict("r")
+  // @Restrict("r")
   public user: UserStore;
   @Restrict()
   name: string = "John Doe";
-  @Restrict("rw")
-  getCredentials = lazy(() => {
-    const credentialStore = new Store();
-    credentialStore.writeEntries({ username: "user1" });
-    return credentialStore;
-  });
+  // @Restrict("rw")
+  // getCredentials = lazy(() => {
+  //   const credentialStore = new Store();
+  //   credentialStore.writeEntries({ username: "user1" });
+  //   return credentialStore;
+  // });
 
   constructor(user: UserStore) {
     super();
--- src/store.ts
+++ src/store.ts
@@ -20,26 +20,93 @@ export interface IStore {
   entries(): JSONObject;
 }
 
-export function Restrict(...params: unknown[]): any {
+// export function Restrict(...params: unknown[]): any {
+// }
+
+export function Restrict(policy: Permission = "none"): any {
+  return function (target: any, propertyKey: string) {   
+    Object.defineProperty(target, propertyKey, {
+      get: function (/*this: Store*/) {
+        return this.read(/*key*/propertyKey)
+      },
+      set: function (this: Store, newVal: StoreValue) {
+        console.log(propertyKey)
+        console.log(newVal)
+        this.policies.set(propertyKey, policy)
+        this.values.set(propertyKey, newVal)
+      },
+      enumerable: true,
+      configurable: true
+    });
+  }
 }
+
 
 export class Store implements IStore {
   defaultPolicy: Permission = "rw";
 
+  policies: Map<string, Permission> = new Map()
+  values: Map<string, StoreValue> = new Map()
+
+  private resolveValue(value: StoreValue): StoreResult {
+    // TODO
+    // if (typeof value === "function")
+      //lazy
+
+    return value as StoreResult
+  }
+
   allowedToRead(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const refOnPermission= this.policies.get(key)
+
+    if (!refOnPermission) {
+      return ["r", "rw"].includes(this.defaultPolicy) 
+        ? true
+        : false
+    }
+
+    if (
+        refOnPermission === "r" ||
+        refOnPermission === "rw"
+      )
+      return true
+    return false
   }
 
   allowedToWrite(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const refOnPermission= this.policies.get(key)
+
+    if (!refOnPermission) {
+      return ["w", "rw"].includes(this.defaultPolicy)
+        ? true
+        : false
+    }
+
+    if (
+        refOnPermission === "w" ||
+        refOnPermission === "rw"
+      )
+      return true
+    return false
+    
+    // throw new Error("Method not implemented.");
   }
 
   read(path: string): StoreResult {
-    throw new Error("Method not implemented.");
+
+    if (this.allowedToRead(path))
+      return this.resolveValue(
+        this.values.get(path)
+      )
+    throw new Error("Not allowed to read");
   }
 
   write(path: string, value: StoreValue): StoreValue {
-    throw new Error("Method not implemented.");
+    
+    this.policies.set(path, this.defaultPolicy)
+    this.values.set(path, value)
+
+    return value
   }
 
   writeEntries(entries: JSONObject): void {
